0f1669a42bd09e67396e3f8614d88b973a5d9ebb
sasivenkatamurthy <sasivenkatamurthyk@gmail.com>
2025-05-18 16:06:58 +0530

    "initial commit"

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ImageGallery from './Componets/ImageGallery';
+
+function App() {
+  return (
+    <div>
+      <h1>📷 Image Gallery</h1>
+      <ImageGallery />
+    </div>
+  );
+}
+
+export default App;
